46c27b875059702070a5cf3f023a9259bb1b58b8
3dgyGamer <jakepark2908@gmail.com>
2025-02-21 14:07:35 -0500

    a'

--- userStory/DataRunner.java
+++ userStory/DataRunner.java
@@ -1,7 +1,30 @@
 import java.util.Scanner;
 
 public class DataRunner {
+  public DataRunner {
+
+  }
+  public static String[] arenaByCapacity(int[] capacities, String[] arenas, int capacity, int flip){
+    String[] greater = new String[capacities.length];
+    String[] lesser = new String[capacities.length];
+    // 1 for greater than, 0 for less than
+   
+    for(int i=0; i < capacities.length; i++){
+      if (capacities[i] <= capacity){
+        lesser[i] = arenas[i];
+      } else {
+        greater[i] = arenas[i];
+      }
+    }
+    if (flip == 0){
+      return lesser;
+    } 
+    return greater;
+    
+  }
   public static void main(String[] args) {
-    FileOperator file = new FileOperator("./capacities.txt");
+    FileOperator capacities = new FileOperator("./capacities.txt");
+    FileOperator arenas = new FileOperator("./arenas.txt");
+
   }
 }
